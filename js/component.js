0af86dc9d5d69e61b9828253bc0813b08ae76e26
devbrnx <chris.makosso@bronx.fr>
2015-06-17 17:18:45 +0200

    Major Bug Fix : Menu UI rendering + code cleanup

--- js/component.js
+++ js/component.js
@@ -29,16 +29,16 @@
         $(document).ready(function () {
             location.hash = '#0';
             nav.wrap('<a class="goto-website" href="http://www.hermes.com/index_fr.html">');
-            nav2.wrap('<a class="goto-next" href="#'+1+'">');
+            nav2.wrap('<a class="goto-next" href="#' + 1 + '">');
 
             slickWrapper.slickElement.on('afterChange', function (event, slick, currentSlide) {
 
-                if (currentSlide == 0 ) {
-                    $('.goto-website').attr('href','http://www.hermes.com/index_fr.html');
+                if (currentSlide == 0) {
+                    $('.goto-website').attr('href', 'http://www.hermes.com/index_fr.html');
                 }
-                else if ($('.goto-website').length > 0 && currentSlide > 0 && currentSlide < 12 ) {
-                    $('.goto-website').attr('href', '#'+(currentSlide-1));
-                    $('.goto-next').attr('href','#'+(currentSlide));
+                else if ($('.goto-website').length > 0 && currentSlide > 0 && currentSlide < 12) {
+                    $('.goto-website').attr('href', '#' + (currentSlide - 1));
+                    $('.goto-next').attr('href', '#' + (currentSlide));
                 }
             });
         });
@@ -116,31 +116,11 @@
      */
     function animateMenu() {
 
-        $('document').ready(function () {
-
             var MenuUi = {
-                cnf: {
-                    viewportsize: function () {
-                       // var $window = $(window);
-                        var size = $(window).width();
-                        return size;
-
-                    },
-                    slideobj: window.onload = function () {
-                        var slideConf = {
-                            'panel': document.getElementById('panel'),
-                            'menu': document.getElementById('menu'),
-                            'padding': 250,
-                            'tolerance': 70
-                        };
-                        return slideConf
-                    }
-                },
-
                 elements: {
                     mainPanel: $('.main-panel'),
                     menu: $('.menu-slide'),
-                    navigationHeader: $('.navbar-header'),
+                    navigationHeader: $('.nav-to-remove'),
                     fluidContainer: $('.container-fluid'),
                     sommaireButton: $('.sommaire'),
                     closeButton: $('#close'),
@@ -150,30 +130,24 @@
                 },
 
                 //Configuration objects ends
-                init: function () {
-                    return MenuUi.renderUI();
-                },
-
-                renderUI: function () {
-                    MenuUi.slideoutUIEvents();
-                    MenuUi.slideoutInit();
-                    MenuUi.BindEvents();
-
-                },
 
                 BindEvents: function () {
-                    if (MenuUi.cnf.viewportsize() > 1222) {
-                        MenuUi.elements.subMenuLastelem.hide();
-                        MenuUi.elements.menu.css({'left': '-50px', 'opacity': '0'});
-                        MenuUi.elements.subMenuElem1.css('paddingLeft', '105px');
-
-                        MenuUi.elements.sommaireButton.on('click', function () {
-                            MenuUi.openMenuUIEvents()
-                        });
-                        MenuUi.elements.closeButton.on('click', function () {
-                            MenuUi.closeMenuUIEvents();
-                        });
-                    }
+                    enquire.register("screen and (min-width: 1222px)", {
+                        match: function () {
+                            MenuUi.elements.subMenuLastelem.hide();
+                            MenuUi.elements.sommaireButton.on('click', function () {
+                                MenuUi.openMenuUIEvents();
+                            });
+                            MenuUi.elements.closeButton.on('click', function () {
+                                MenuUi.closeMenuUIEvents();
+                            });
+                        },
+                        unmatch: function () {
+                            MenuUi.elements.subMenuLastelem.show();
+                            MenuUi.elements.sommaireButton.off();
+                            MenuUi.elements.closeButton.off();
+                        }
+                    });
                 },
 
                 openMenuUIEvents: function () {
@@ -214,7 +188,7 @@
                             height: '150px'
                         }, 750);
                         //ends
-                    }, 1000)
+                    }, 1000);
                     setTimeout(function () {
                         MenuUi.elements.sommaireButton.animate({
                             opacity: '1',
@@ -225,58 +199,62 @@
                 },
 
                 slideoutUIEvents: function () {
-
-                    if (MenuUi.cnf.viewportsize() < 1221) {
-                        MenuUi.elements.mainPanel
-                            .attr('id', 'panel')
-                            .prepend(
-                            '<nav class="navbar navbar-default toggle-button">' +
-                            '<div class="navbar-header">' +
-                            '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"  aria-expanded="false" aria-controls="navbar">' +
-                            '<span class="sr-only">Toggle navigation</span>' +
-                            '<span class="icon-bar"></span>' +
-                            '<span class="icon-bar"></span>' +
-                            '<span class="icon-bar"></span>' +
-                            '</button>' +
-                            '</div>' +
-                            '</nav>'
-                        );
-                        MenuUi.elements.menu.css({'height': 'auto', 'background-color': '#f5f5f5'}).attr('id', 'menu');
-                        MenuUi.elements.navigationHeader.remove();
-                        MenuUi.elements.fluidContainer.css({'padding-left': '1px'});
-
-                        if (MenuUi.cnf.viewportsize() < 769) {
-                            MenuUi.elements.menu.css('top', '125px');
-                            MenuUi.elements.mainPanel.css({'height': 'auto'});
+                    enquire.register("screen and (max-width: 1221px)", {
+                        match: function () {
+                            MenuUi.elements.menu.attr('id', 'menu');
+                            MenuUi.elements.mainPanel
+                                .attr('id', 'panel');
+                            console.log('added');
+                        },
+                        unmatch: function () {
+                            MenuUi.elements.menu.removeAttr('id');
+                            MenuUi.elements.mainPanel
+                                .removeAttr('id');
+                            console.log('removed');
                         }
-                    }
-                    $(window).on('resize', function(){
-
-                    })
+                    });
                 },
 
                 slideoutInit: function () {
-                    try {
-                        var slideout = new Slideout(MenuUi.cnf.slideobj());
-                        slideout.disableTouch();
-                        $('.toggle-button').on('click', function () {
-                            slideout.toggle();
-                            MenuUi.elements.mainPanel.css({
-                                'height': '610',
-                                'background-color': '#ffffff',
-                                'box-shadow': '-5px 5px 8px 0px rgba(50, 50, 50, 0.2)'
-                            });
-                        });
-                    } catch (e) {
+                    enquire.register("screen and (max-width: 1221px)", {
+                        match: function () {
+                            if(typeof slideout === "undefined"){
+                                var slideout = new Slideout(
+                                    {
+                                        'panel': document.getElementById('panel'),
+                                        'menu': document.getElementById('menu'),
+                                        'padding': 250,
+                                        'tolerance': 70
+                                    }
+                                );
+                                slideout.disableTouch();
+                                $('.to-hide').on('click', function () {
+                                    console.log('slider clicked');
+                                    slideout.toggle();
+                                });
+                            }
+                        },
+                        unmatch: function () {
+                            $('#navbar>ul').removeClass('slideout-menu');
+                            $('.main-panel').removeClass('slideout-panel');
+                        }
+                    });
+                },
 
-                    }
-
+                //Rendering and executing the menu
+                renderUI: function () {
+                    MenuUi.slideoutUIEvents();
+                    MenuUi.slideoutInit();
+                    MenuUi.BindEvents();
+                },
+                init: function () {
+                    return MenuUi.renderUI();
                 }
+
             };
 
-            MenuUi.init();
-
-        });
+        //Let's load init menu
+          MenuUi.init();
 
     }
 
@@ -301,17 +279,12 @@
                 element = $('.sommaire');
             element.drawText(conf);
         });
-
-
     }
 
     function handleClasses() {
-
         $('button').on('click', function () {
             $('.icon-bar').toggleClass('icon-bar-color');
         });
-
-
     }
 
 
@@ -327,10 +300,9 @@
         animateMenu();
         canvasDraw();
         handleClasses();
-
     }
 
     $(window).ready(function () {
         load_functions();
     });
-})(jQuery)
+})(jQuery);
